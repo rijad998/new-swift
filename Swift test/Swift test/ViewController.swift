e11a69bbd970b5fbcc18f46c161f35b33b9de219
Rijad Babovic <rijad.babovic@hotmail.com>
2019-05-24 12:19:05 +0200

    init project

---
+++ Swift test/Swift test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift test
+//
+//  Created by Rijad Babovic on 24/05/2019.
+//  Copyright © 2019 Rijad Babovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
